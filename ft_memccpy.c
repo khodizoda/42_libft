1d74e51ebcf3df9d7a068ec85526bece20f9abec
Gulnozai Khodizoda <gkhodizo@e1z3r6p6.42.us.org>
2020-02-19 14:08:29 -0800

    memccpy

---
+++ ft_memccpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkhodizo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/19 13:45:34 by gkhodizo          #+#    #+#             */
+/*   Updated: 2020/02/19 13:45:35 by gkhodizo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *restrict dst, const void *restrict src,
+	int c, size_t n)
+{
+	size_t	i;
+	char	*cdst;
+	char	*csrc;
+
+	i = 0;
+	cdst = (char *)dst;
+	csrc = (char *)src;
+	while (i < n)
+	{
+		if (csrc[i] == (unsigned char)c)
+			return (&dst[i + 1]);
+		cdst[i] = csrc[i];
+		i++;
+	}
+	return (NULL);
+}
