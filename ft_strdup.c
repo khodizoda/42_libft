af8875b43153cf21b78c78160871ca635f97c14d
Gulnozai Khodizoda <gkhodizo@e1z3r6p6.42.us.org>
2020-02-19 22:45:26 -0800

    strdup

---
+++ ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkhodizo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/19 22:02:56 by gkhodizo          #+#    #+#             */
+/*   Updated: 2020/02/19 22:02:58 by gkhodizo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	i;
+	size_t	len;
+	char	*tmp;
+
+	i = 0;
+	len = ft_strlen(s1);
+	tmp = (char *)malloc(len * sizeof(s1));
+	if (!tmp)
+		return (NULL);
+	while (s1[i])
+	{
+		tmp[i] = s1[i];
+		i++;
+	}
+	tmp[i] = '\0';
+	return (tmp);
+}
