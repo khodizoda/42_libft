aab60ce3fb58a43cadc148b80bebd88c8eef96ab
Gulnozai Khodizoda <gkhodizo@e1z3r6p9.42.us.org>
2020-02-21 15:04:43 -0800

    upd

--- Makefile
+++ Makefile
@@ -22,9 +22,11 @@ CCFLAGS = -g -Wall -Wextra -Werror -I./libft.h
 
 HEADER = ./libft.h
 
-NAMES = ./ft_memset ./ft_bzero ./ft_memcpy ./ft_memccpy ./ft_memmove \
-		./ft_memchr ./ft_memcmp ./ft_strlen ./ft_strdup ./ft_strcpy \
-		./ft_strncpy ./tests
+NAMES = ./ft_memset ./ft_bzero ./ft_memcpy ./ft_memccpy ./ft_memmove ./ft_memchr \
+		./ft_memcmp ./ft_strlen ./ft_strdup ./ft_strcpy ./ft_strncpy ./ft_strcat \
+		./ft_strncat ./ft_strlcat ./ft_strchr ./ft_strrchr ./ft_strstr \
+		./ft_strnstr ./ft_strcmp ./ft_strncmp ./ft_atoi ./ft_isalpha ./ft_isdigit \
+		./ft_isalnum ./ft_isascii ./ft_isprint ./ft_toupper ./ft_tolower ./test \
 
 SRCS = $(addsuffix .c, $(NAMES))
 
--- libft.h
+++ libft.h
@@ -17,6 +17,7 @@
 # include <string.h>
 # include <unistd.h>
 # include <stdio.h>
+# include <ctype.h>
 
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
@@ -30,5 +31,22 @@ size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
+char	*ft_strcat(char *restrict s1, const char *restrict s2);
+char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n);
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strstr(const char *haystack, const char *needle);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(const char *str);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
