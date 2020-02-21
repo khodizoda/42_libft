1969c54382d2a64749bfea9a1cdc88a6b60956cb
Gulnozai Khodizoda <gkhodizo@e1z3r6p6.42.us.org>
2020-02-20 18:36:59 -0800

    strstr

---
+++ ft_strstr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkhodizo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/20 17:55:27 by gkhodizo          #+#    #+#             */
+/*   Updated: 2020/02/20 17:55:29 by gkhodizo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	int		pos;
+	char	*h;
+
+	pos = 0;
+	h = (char *)haystack;
+	if (!*needle)
+		return (h);
+	while (*h)
+	{
+		needle = needle - pos;
+		while (*h == *needle)
+		{
+			h++;
+			needle++;
+			pos++;
+			if (!(*needle))
+				return (h - pos);
+		}
+		h = h - pos;
+		h++;
+	}
+	return (NULL);
+}
