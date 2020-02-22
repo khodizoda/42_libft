9394f3ed2707d31a4789e3007bdfac24584a91a6
Gulnozai Khodizoda <gkhodizo@e1z3r6p9.42.us.org>
2020-02-21 20:03:40 -0800

    allocated mem to new str

---
+++ ft_strnew.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkhodizo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/21 19:31:31 by gkhodizo          #+#    #+#             */
+/*   Updated: 2020/02/21 19:31:33 by gkhodizo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnew(size_t size)
+{
+	size_t	i;
+	char	*new;
+
+	i = 0;
+	new = (char *)malloc(sizeof(size + 1));
+	if (!new)
+		return (NULL);
+	else
+	{
+		while (i < size + 1)
+		{
+			new[i] = '\0';
+			i++;
+		}
+		new[i] = '\0';
+		return (new);
+	}
+	return (NULL);
+}
