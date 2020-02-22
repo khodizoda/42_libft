c2c9510cfb76a34fe05c77e67cf140edd513fb8d
Gulnozai Khodizoda <gkhodizo@e1z3r6p9.42.us.org>
2020-02-21 17:50:31 -0800

    malloc implementation

---
+++ ft_memalloc.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkhodizo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/21 16:02:36 by gkhodizo          #+#    #+#             */
+/*   Updated: 2020/02/21 16:02:38 by gkhodizo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memalloc(size_t size)
+{
+	size_t			i;
+	unsigned char	*mem;
+
+	if (size > 0 && size < ULONG_MAX)
+	{
+		mem = (unsigned char *)malloc(sizeof(size + 1));
+		if (!mem)
+			return (NULL);
+		else
+		{
+			i = -1;
+			while (++i < size + 1)
+				mem[i] = 0;
+			return (mem);
+		}
+	}
+	return (NULL);
+}
