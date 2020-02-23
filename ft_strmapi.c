8efa497a094a04be316022b600b1263f86334e02
Gulnozai Khodizoda <gkhodizo@e1z3r6p6.42.us.org>
2020-02-23 14:38:14 -0800

    strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -12,4 +12,23 @@
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	size_t			len;
+	char			*new;
+	char			*cs;
+
+	i = 0;
+	len = ft_strlen(s);
+	new = (char *)malloc(sizeof(len + 1));
+	cs = (char *)s;
+	if (!new)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		new[i] = f(i, cs[i]);
+		i++;
+	}
+	return (new);
+}
