1450784ea9ce0a2d16ff1963cd044bdc8f62e58a
Gulnozai Khodizoda <gkhodizo@e1z3r6p6.42.us.org>
2020-02-23 14:39:43 -0800

    upd

--- libft.h
+++ libft.h
@@ -16,6 +16,8 @@
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
+# include <limits.h>
+# include <stdio.h>
 
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
