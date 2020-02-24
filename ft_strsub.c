23ca6584e35c9837662028f82de9f0845586fa25
Gulnozai Khodizoda <gkhodizo@e1z3r6p6.42.us.org>
2020-02-23 19:00:37 -0800

    adds str to substr

---
+++ ft_strsub.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkhodizo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/23 18:21:15 by gkhodizo          #+#    #+#             */
+/*   Updated: 2020/02/23 18:21:17 by gkhodizo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*sub;
+
+	i = 0;
+	sub = (char *)malloc(sizeof(len + 1));
+	if (!sub)
+		return (NULL);
+	while (i < len && s[start] != '\0')
+	{
+		sub[i] = s[start];
+		i++;
+		start++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
